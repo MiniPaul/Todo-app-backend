d6181164758883a3b7456b0648da28bab3838830
MiniPaul <minipaul1227@gmail.com>
2024-03-01 10:53:43 +0530

    todo app backend created

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SignupModule } from './signup/signup.module';
+import { ListModule } from './list/list.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://minipaul:minipaul@cluster0.isuura7.mongodb.net/TodoDb?retryWrites=true&w=majority&appName=Cluster0'),
+    SignupModule, ListModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
